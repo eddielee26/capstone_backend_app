e13f21d097dae51a8f0cebf7e54a78ab3b186014
eddie <eddielee001@gmail.com>
2019-10-28 21:42:44 -0500

    adds validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,4 +2,9 @@ class Exercise < ApplicationRecord
   belongs_to :user
   has_many :workout_exercises
   has_many :workouts, through: :workout_exercises
+
+  validates :name, presence: true
+  validates :sets, numericality: { greater_than: 0 }
+  validates :reps, numericality: { greater_than: 0 }
+  validates :weight, numericality: { greater_than: 0 }
 end
